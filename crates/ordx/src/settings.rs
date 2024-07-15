7edaa92796f7fec17f91ca81b6ea419167fd19dd
iota9star <iota9star@foxmail.com>
2024-07-15 22:21:41 +0800

    ✨ Use simple env

--- crates/ordx/src/settings.rs
+++ crates/ordx/src/settings.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{env, fmt};
 use std::fmt::{Display, Formatter};
 
 use config::Config;
@@ -13,10 +13,6 @@ pub struct Settings {
     pub bitcoin_rpc_username: Option<String>,
     pub bitcoin_rpc_password: Option<String>,
     pub max_block_queue_size: Option<u8>,
-    pub cargo_pkg_version: String,
-    pub vergen_build_timestamp: String,
-    pub vergen_cargo_target_triple: String,
-    pub vergen_rustc_semver: String,
 }
 
 impl Display for Settings {
@@ -29,14 +25,22 @@ impl Display for Settings {
         bitcoin_rpc_username: {}\n\
         bitcoin_rpc_password: {} \n\
         max_block_queue_size: {}\n\
-        cargo_pkg_version: {}\n\
-        vergen_build_timestamp: {}\n\
-        vergen_cargo_target_triple: {}\n\
-        vergen_rustc_semver: {}\n\
-        ========================================", self.network.clone().unwrap_or_default(), self.data_dir.clone().unwrap_or_default(), self.bitcoin_rpc_url.clone().unwrap_or_default(), self.bitcoin_rpc_username.as_ref().map(|_| "***").unwrap_or_default(), self.bitcoin_rpc_password.as_ref().map(|_| "********").unwrap_or_default(), self.max_block_queue_size.map(|x| x.to_string()).unwrap_or_default(), self.cargo_pkg_version,
-               self.vergen_build_timestamp,
-               self.vergen_cargo_target_triple,
-               self.vergen_rustc_semver)
+        build_version: {}\n\
+        build_timestamp: {}\n\
+        target_triple: {}\n\
+        rustc_semver: {}\n\
+        ========================================",
+               self.network.clone().unwrap_or_default(),
+               self.data_dir.clone().unwrap_or_default(),
+               self.bitcoin_rpc_url.clone().unwrap_or_default(),
+               self.bitcoin_rpc_username.as_ref().map(|_| "***").unwrap_or_default(),
+               self.bitcoin_rpc_password.as_ref().map(|_| "********").unwrap_or_default(),
+               self.max_block_queue_size.map(|x| x.to_string()).unwrap_or_default(),
+               env!("CARGO_PKG_VERSION"),
+               env!("VERGEN_BUILD_TIMESTAMP"),
+               env!("VERGEN_CARGO_TARGET_TRIPLE"),
+               env!("VERGEN_RUSTC_SEMVER"),
+        )
     }
 }
 
